dbd9d1495ed19f4d673200fba5a5f6101b25fda9
Renato Pessoa <renatopessoaneto@gmail.com>
2020-10-16 11:58:47 -0300

    inicio projeto spring

---
+++ nosso-banco-digital/src/test/java/io/github/pessoas/nossobancodigital/NossoBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pessoas.nossobancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
